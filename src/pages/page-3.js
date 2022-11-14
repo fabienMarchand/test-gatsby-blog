60f0669b184a6bd042ff095ef26041bdbbdc60c0
Marcha <marchaf@gmail.com>
2022-11-14 12:33:32 +0100

    adding all mu new blog test

---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hi from the third page</h1>
+    <p>Welcome to page 3</p>
+  </Layout>
+)
+
+
+export default ThirdPage
